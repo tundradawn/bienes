f571d98ee5faf9ce1a2554e4d010a34d08566c21
Darren Place <48530717+darrenplace@users.noreply.github.com>
2020-04-07 00:33:18 -0500

    impliment elipse art (exponential)
    peer programmed by @danieltamkin

--- ellipse/sketch.js
+++ ellipse/sketch.js
@@ -1,7 +1,57 @@
+let test;
+let elements = [];
 function setup() {
-
+  canvasWidth = 800;
+  canvasHeight = 600;
+  createCanvas(canvasWidth, canvasHeight);
+  ellipseSize = 0;
+  ellipseOpacity = 1;
+  frameCounter = 0;
+  elements.push(new simpleEllipse(10, 10, 100, 1));
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  background(0);
+  for(let i = 0; i < elements.length; i++){
+    elements[i].done(function(birth){
+      console.log('dead');
+      elements[i] = new simpleEllipse(10, 10, 100, 1);
+      elements.push(new simpleEllipse(10, 10, 100, 1));
+    }, function(){  
+     elements[i].animate();
+     elements[i].display();
+    });
+  }
+
+}
+
+class simpleEllipse {
+  constructor(x, y, size, opacity){
+    this.x = Math.floor(Math.random() * canvasWidth);
+    this.y = Math.floor(Math.random() * canvasHeight);
+    this.opacity = opacity;
+    this.size = size;
+  }
+  
+  animate() {
+    this.size = this.size +1;
+    this.opacity = this.opacity - 0.005;
+    if(this.opacity < 0) {
+      this.opacity = 0;
+      this.dead = true;
+    }
+  }
+  done(callback, notdone){
+    if(this.dead){
+      callback();
+    }
+    else{
+      notdone();
+    }
+  }
+  display() {
+    stroke("rgba(255,255,255," + this.opacity + ')');
+    fill(0,0);
+    ellipse(this.x, this.y, this.size, this.size);
+  }
 }
